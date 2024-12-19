da6f8c27f5b2d3a40155a31baf309b3c0520c5f9
Timalexanderandersson <timalexanderandersson@hotmail.com>
2024-12-19 19:11:20 +0100

    created models for gallery and migrated

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+
+    image = models.ImageField(upload_to=None)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+     return f'{self.title} made around {self.created_at}'
+
