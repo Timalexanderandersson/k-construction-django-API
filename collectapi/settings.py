93fde9076f212325bbd472a024648a0a43cc3136
Timalexanderandersson <timalexanderandersson@hotmail.com>
2024-12-23 18:30:29 +0100

    created views for sending offert in views and settings

--- collectapi/settings.py
+++ collectapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import env
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
--- offert/views.py
+++ offert/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .serializers import OffertSerializers
-from rest_framework import generics
+from rest_framework import generics, status 
+from django.http import Http404
+from rest_framework.response import Response
+from django.core.mail import send_mail
+
 
 from .models import Offert
 
@@ -9,4 +13,30 @@ from .models import Offert
 class PostOffert(generics.ListCreateAPIView):
     serializer_class = OffertSerializers
     queryset = Offert.objects.all()
+
+    def post(self, request):
+      serializer = OffertSerializers(data=request.data)
+      if serializer.is_valid():
+        mail = serializer.save()
+        self.send_email(mail)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def send_email(self, mail):
+       subject = f'Offert from: {mail.name}'
+       message = (
+        f'Phone number: {mail.phone_number}\n\n'
+        f'Category: {mail.option_service}\n\n'
+        f'Work description: {mail.job_description}\n'
+        f'Mail sent from: {mail.epost}\n\n'
+       )
+       from_email = 'from@gmail.com'
+       recipient_list = ["timovetim@gmail.com"]
+
+       send_mail(
+         subject,
+         message,
+         from_email,
+         recipient_list,
+         fail_silently=False,
+        )
